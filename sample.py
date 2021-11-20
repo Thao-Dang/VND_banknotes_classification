9f8c986251dfef17cc01211eec674b9e0b67612e
Thao-Dang <90619603+Thao-Dang@users.noreply.github.com>
2021-11-21 00:23:56 +0700

    update file sample
    upload model predict money

--- sample.py
+++ sample.py
@@ -1,9 +1,18 @@
+from numpy.core.fromnumeric import shape
 import streamlit as st
 import pandas as pd
 import numpy as np
 import cv2
+import tensorflow as tf
 
-menu = ['home', 'about me', 'read data', 'camera']
+model = tf.keras.models.load_model('model\my_model_checkpoint.h5')
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+money_type = ['1000', '10000', '100000', '2000', '20000', '200000', '5000', '50000', '500000']
+  
+
+menu = ['home', 'about me', 'read data', 'camera', 'predict money']
 choice = st.sidebar.selectbox('Menu', menu)
 if choice == 'home':
     st.write('Hello')
@@ -22,6 +31,25 @@ elif choice == 'read data':
 elif choice == 'about me':
     fileup = st.file_uploader('upload file', type = ['jpg', 'png', 'jpeg'])
     st.image(fileup)
+
+elif choice == 'predict money':
+    image_upload = st.file_uploader('upload file', type = ['jpg', 'png', 'jpeg'])
+    if image_upload != None:
+        image_np = np.asarray(bytearray(image_upload.read()),dtype = np.uint8)
+        img = cv2.imdecode(image_np,1)
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (224,224))
+        img = np.expand_dims(img, axis=0)
+        prediction = model.predict(img)
+        index = np.argmax(prediction[0])
+        money = money_type[index]
+        st.image(image_upload)
+        st.write('This is:', money)
+        
+        
+
+
+
 elif choice == 'camera':
     cam = cv2.VideoCapture(0) # device 0. If not work, try with 1 or 2
 
@@ -40,3 +68,4 @@ elif choice == 'camera':
 
     cam.release()
     cv2.destroyAllWindows()
+    
