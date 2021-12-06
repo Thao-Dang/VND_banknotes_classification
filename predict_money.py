720c38bf0cb8dc1e2de4e9815fb94ffedb497404
Thao-Dang <90619603+Thao-Dang@users.noreply.github.com>
2021-12-06 22:33:44 +0700

    clean unnecessary lines

--- predict_money.py
+++ predict_money.py
@@ -14,23 +14,7 @@ money_type = ['1000', '10000', '100000', '2000', '20000', '200000', '5000', '500
 st.header('PREDICT VIETNAMESE DONG')
 menu = ['Take photo by webcam', 'Upload a photo']
 choice = st.sidebar.selectbox('Ways to input image', menu)
-# if choice == 'home':
-#     st.write('Hello')
-#     st.header('First webapp')
-#     st.image('media\dog-beach-lifesaver.png')
-#     col1, col2 = st.columns(2)
-#     with col1:
-#         dog_name = st.text_input('What is your dog name?')
-#         st.write('Your dog name:', dog_name)
-#     with col2:
-#         age = st.slider('Dog age:', min_value=1, max_value=20)
-#         st.write('Your dog age:', age)
-# if choice == 'read data':
-#     df = pd.read_csv('media\AB_NYC_2019.csv')
-#     st.dataframe(df)
-# elif choice == 'about me':
-#     fileup = st.file_uploader('upload file', type = ['jpg', 'png', 'jpeg'])
-#     st.image(fileup)
+
 if choice == 'Take photo by webcam':
     st.write('Click on "Capture" button')
     cam = cv2.VideoCapture(0) # device 0. If not work, try with 1 or 2
